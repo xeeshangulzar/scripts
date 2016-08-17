e9e110c9ffba3c61c517387d431d6d5622e6536c
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2016-08-17 16:19:39 +0500

    Sqlite forced index

--- sqlite3.rb
+++ sqlite3.rb
@@ -4,3 +4,7 @@ sqlite3 database_path #path: db/development.sqlite3
 .tables #select all tables
 pragma table_info(tabke_name); #get table structure
 .fullschema #get full schema, columns, indexes etc etc
+
+#sqlite forced_index INDEXED BY index_name
+sql = "SELECT 'articles'.* FROM 'articles' INDEXED BY index_articles_on_updated_at WHERE 'articles'.'name' = 'my' AND ('articles'.'updated_at' BETWEEN '2016-08-07 10:30:15.447342' AND '2016-08-16 10:30:15.452104')";
+db_results = ActiveRecord::Base.connection.execute(sql)
